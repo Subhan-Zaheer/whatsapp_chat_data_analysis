6adbb668c8c55b99c637b22f3c000b7dfa787b80
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-09-02 00:32:07 +0500

    Convered raw string to dataframe

--- main.py
+++ main.py
@@ -1,19 +1,86 @@
 import asyncio
 import os
+import re
+
 import numpy as np
 import pandas as pd
 import matplotlib as mlt
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 
-def read_data(file_name):
-    # print(os.getcwd())
-    # os.chdir(r"C:\Users\Khan Mob s  Comp\Desktop")
-    # print(os.getcwd())
+def converting_time_in_12hr_format():
+    return '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[APap][mM]\s-\s'
+
+def converting_time_in_24hr_format():
+    return '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+
+def date_time_12hr_format():
+    return '%d/%m/%Y, %I:%M %p - '
+
+def date_time_24hr_format():
+    return '%d/%m/%Y, %H:%M - '
+
+
+
+def creating_dataFrame(file_name):
+
+    """ Converting raw data into presentable data frame"""
+
+    split_formats = {
+        '12hr': converting_time_in_12hr_format() ,
+        '24hr': converting_time_in_24hr_format(),
+        'custom': ''
+    }
+    datetime_formats = {
+        '12hr': date_time_12hr_format(),
+        '24hr': date_time_24hr_format(),
+        'custom': ''
+    }
+
     file = open(r"bds_chat_data.txt", 'r', encoding='utf-8')
-    # print(file)
-    raw_string = "\n".join(file.read().split('\n'))
-    print(raw_string)
+
+    raw_string = " ".join(file.read().split('\n'))   # splitting our text by \n.
+
+    # extracting the user messages from given raw string.
+    user_messages = re.split(split_formats['12hr'], raw_string)[1:]
+
+    # extracting out the date and time from given raw string.
+    date_and_time = re.findall(split_formats['12hr'], raw_string)
+    # print(date_and_time)
+    chats_dataFrame = pd.DataFrame({'date_time':date_and_time, 'user_messages':user_messages })
+    # print(chats_dataFrame)
+
+    # Converting date_time to pandas date_time object.
+    chats_dataFrame['date_time'] = pd.to_datetime(chats_dataFrame['date_time'], format=datetime_formats['12hr'])
+
+    # print(chats_dataFrame)
+
+    user_name = []
+    message = []
+
+    for i in chats_dataFrame['user_messages']:
+
+        a = re.split('([\w\W]+?):\s', i)
+        if(a[1:]):
+            # creating two different lists of usernames and messages from user_message column
+            # if a row is a valid message than it will store message in message list and
+            # user_name in user_name list
+            user_name.append(a[1])
+            message.append(a[2])
+        else:
+            # if it is not a valid message than it would be a group notification
+            # we will also store it in list accordingly.
+            user_name.append("Group Notification")
+            message.append(a[0])
+
+    chats_dataFrame['User_Name'] = user_name
+    chats_dataFrame['Message'] = message
+    chats_dataFrame.drop('user_messages', inplace=True, axis=1)
+    print(chats_dataFrame)
+
+
+
     # line = file.readlines()
     # print(line.__sizeof__())
     # splitting = line[0].split(',')
@@ -30,7 +97,8 @@ def read_data(file_name):
 
 if __name__ == '__main__':
     file_name = input("Enter file name with extension : ")
-    read_data(file_name)
-    print("\n-------------*********************------------------\n")
-    read_data(file_name)
+    creating_dataFrame(file_name)
+    # print("\n-------------*********************------------------\n")
+    # creating_dataFrame(file_name)
+
     pass
